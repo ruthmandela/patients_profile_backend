94cd4500380ef60a639fc4f7d7881a7c2fe5a474
ruth <ruth.atieno@giktek.io>
2024-06-24 10:02:15 +0300

    update

--- src/main/java/com/example/patents/Controller/PatientRegistrationController.java
+++ src/main/java/com/example/patents/Controller/PatientRegistrationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping( "/patient")
 
 
+
 public class PatientRegistrationController {
     @Autowired
     PatientRegistrationService patientRegistrationService;
